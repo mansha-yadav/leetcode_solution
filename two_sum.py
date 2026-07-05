8658d61dbb201e1b909357bb45849f0f30616adc
mansha yadav <196092615+mansha-yadav@users.noreply.github.com>
2026-07-05 16:12:51 +0530

    two sum values

--- two_sum.py
+++ two_sum.py
@@ -47,3 +47,9 @@ class Solution:
             seen[num] = i
 
         return []
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.twoSum([2, 7, 11, 15], 9))   # [0, 1]
+    print(sol.twoSum([3, 2, 4], 6))         # [1, 2]
+    print(sol.twoSum([3, 3], 6))            # [0, 1]
